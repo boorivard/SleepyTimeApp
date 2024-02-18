a9ad768866bf065ee7061da600147d7c781876ea
boorivard <boorivard@comcast.net>
2024-02-18 16:25:48 -0500

    Initial Commit

---
+++ SleepyTimeApp/SleepyTimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SleepyTimeAppApp.swift
+//  SleepyTimeApp
+//
+//  Created by Jared Rivard on 2/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SleepyTimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SleepyTimeAppUITests/SleepyTimeAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SleepyTimeAppUITestsLaunchTests.swift
+//  SleepyTimeAppUITests
+//
+//  Created by Jared Rivard on 2/18/24.
+//
+
+import XCTest
+
+final class SleepyTimeAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
