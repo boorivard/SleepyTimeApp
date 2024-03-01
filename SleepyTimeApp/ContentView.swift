055da64fdbe3feef6d33efab9463d026f572f505
boorivard <boorivard@comcast.net>
2024-03-01 13:41:05 -0500

    fixed some basic stuff
    moved Splash Screen to new View file
    made so Splash Screen is called in app file.

--- SleepyTimeApp/ContentView.swift
+++ SleepyTimeApp/ContentView.swift
@@ -23,48 +23,4 @@ struct ContentView: View {
     ContentView()
 }
 
-// Struct for Splash Screen
-struct SplashScreenView: View {
-    @State private var isActive = false
-    @State private var scale: CGFloat = 0.8
-    @State private var systemImageOpacity = 0.0
-    @State private var imageOpacity = 1.0
-    
-    var body: some View {
-        ZStack {
-            if isActive {
-                ContentView()
-                    .transition(.scale)
-            } else {
-               LinearGradient(gradient: Gradient(colors: [Color.blue, Color.black]), startPoint: .top, endPoint: .bottom)
-            // Color.black.ignoresSafeArea()  //Alternate color choice: Plain Black
-                
-                    .ignoresSafeArea()
-                                   
-                VStack {
-                    Image("logo")  //logo is the sleepytime logo in assets
-                        .resizable()
-                        .scaledToFit()
-                        .opacity(imageOpacity)
-                        .frame(width: 125, height: 125)
-                        .padding()
-                        .shadow(color: .black, radius: 5, x: 0, y: 5)
-                }
-                
-               .scaleEffect(scale)
-               .transition(.scale)
-                .onAppear {
-                    withAnimation(.easeInOut(duration: 0.9)) {
-                        scale = 1
-                        systemImageOpacity = 1
-                   }
-                }
-            }
-        }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.8) {
-                    isActive = true
-            }
-        }
-    }
-}
+
