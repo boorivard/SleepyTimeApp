6b1838ce1291ac0ef2d041997f6d23a58c28aeff
Nick-Schiffer <160027436+Nick-Schiffer@users.noreply.github.com>
2024-03-13 00:13:01 -0400

    Alarm stats calculation

--- SleepyTimeApp/ContentView.swift
+++ SleepyTimeApp/ContentView.swift
@@ -355,4 +355,34 @@ struct ContentView_Previews: PreviewProvider {
     }
 
 }
+struct alarmstats {
+    var hour: Int
+    var minute: Int
+}
+//Function to calculate total alarm time
+func Totalalarmtime(alarm: alarmstats, Wakeuptime: Date) -> TimeInterval {
+    let calendar = Calendar.current
+    
+    
+    //Get current time & date
+    _ = Date()
+    let WakeupComponents = calendar.dateComponents([.year, .month, .day], from: Wakeuptime)
+    
+    // New date representing the alarm time for the current day
+    var alarmDatecomponents = DateComponents()
+    alarmDatecomponents.year = WakeupComponents.year
+    alarmDatecomponents.month = WakeupComponents.month
+    alarmDatecomponents.day = WakeupComponents.day
+    alarmDatecomponents.hour = alarm.hour
+    alarmDatecomponents.minute = alarm.minute
+    
+    let alarmTime = calendar.date(from: alarmDatecomponents)!
+    
+    //calculation for time slept
+    let timeslept = Wakeuptime.timeIntervalSince(alarmTime)
+    
+    return timeslept
+    
+    
+}
 
