2a7e3961a256c9d9963ca6849e4ab072517d7e26
boorivard <boorivard@comcast.net>
2024-03-26 14:42:51 -0400

    set up firebase basics

--- SleepyTimeApp/SleepyTimeAppApp.swift
+++ SleepyTimeApp/SleepyTimeAppApp.swift
@@ -5,9 +5,21 @@
 //  Created by Jared Rivard on 2/18/24.
 //
 
+import FirebaseCore
+import FirebaseFirestore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+    let db = Firestore.firestore()
+    return true
+  }
+}
 //Jared Rivard's code is here
 @main struct SleepyTimeAppApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
             SplashScreen()
