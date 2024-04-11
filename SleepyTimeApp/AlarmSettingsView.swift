78ef6e456630d89028c121ba109959270e171447
Nick-Schiffer <160027436+Nick-Schiffer@users.noreply.github.com>
2024-04-11 13:59:32 -0400

    Alarm settings: update
    added the sleepymode delay timer

--- SleepyTimeApp/AlarmSettingsView.swift
+++ SleepyTimeApp/AlarmSettingsView.swift
@@ -12,13 +12,15 @@ struct AlarmSettingsView: View {
     @Binding var alarmTime: Date
     @Binding var isAlarmOn: Bool
     @Binding var isAlarmTriggered: Bool
-
+    @State private var selectedDelay: TimeInterval = 300   //defaulted sleepytime mode delay to 5 minutes
+    @State private var sleepytimeTimer: Timer?
+    
     var body: some View {
         VStack {
             Spacer()
             Text("Wake up")
                 .font(.system(size: 75, weight: .bold))
-
+            
             Button(action: {
                 snoozeAlarm()
                 Sounds.stopSound()
@@ -32,14 +34,14 @@ struct AlarmSettingsView: View {
                     .background(Color.green)
                     .cornerRadius(100)
             }
-
+            
             Slider(value: $snoozeDuration, in: 10...600, step: 10) {
                 Text("Snooze Timer: \(Int(snoozeDuration)) seconds")
             }
             .padding()
-
+            
             Spacer()
-
+            
             Button(action: {
                 Sounds.stopSound()
                 isSleepModeActive.toggle()
@@ -53,10 +55,40 @@ struct AlarmSettingsView: View {
             .foregroundColor(.white)
             .cornerRadius(100)
         }
+         Button(action: {
+            // Start the timer with the selected delay
+            startSleepytimeTimer(withDelay: selectedDelay)
+        }) {
+            Text("Set Delay for Sleepytime Mode")
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding()
+        }
+        
+        //picker for sleepytime mode delay
+        Picker("Select Delay", selection: $selectedDelay) {
+            Text("1 minute").tag(60)
+            Text("5 minutes").tag(300)
+            Text("10 minutes").tag(600)
+            Text("15 minutes").tag(900)
+            Text("20 minutes").tag(1200)
+        }
+        
+        .pickerStyle(SegmentedPickerStyle())
+        .padding()
+        
+        Spacer()
+        
     }
-
     func snoozeAlarm() {
         alarmTime = Date().addingTimeInterval(snoozeDuration)
+    }
+    func startSleepytimeTimer( withDelay delay: TimeInterval) {
+        sleepytimeTimer?.invalidate()  //invalidates any other timer
+        sleepytimeTimer = Timer.scheduledTimer(withTimeInterval: delay, repeats: false)
+        {_ in
+            isSleepModeActive.toggle()
+        }
     }
 }
 
@@ -74,3 +106,5 @@ struct AlarmSettingsView: View {
     
     
 }  */
+
+
